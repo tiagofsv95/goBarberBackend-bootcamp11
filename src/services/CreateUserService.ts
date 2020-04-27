0beafadee06e1e0c0b0991fe3f24d6415df5f894
Tiago Felipe <tiagofsv95@gmail.com>
2020-04-26 23:01:38 -0300

    Commit changes

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
 import User from '../models/User';
 
 interface Request {
@@ -18,7 +19,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('Email address already exist.');
+      throw new AppError('Email address already exist.', 400);
     }
 
     const hashedPassword = await hash(password, 8);
