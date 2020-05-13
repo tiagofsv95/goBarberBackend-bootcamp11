b38b408a906e2a78e30b5b15c15094588d5b1d67
Tiago Felipe <tiagofsv95@gmail.com>
2020-05-13 09:14:45 -0300

    Commit changes

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: '8ec6b89f2e22d5316852147740d6dc40',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { celebrate, Segments, Joi } from 'celebrate';
 import uploadConfig from '@config/upload';
 
 import UsersControllers from '@modules/users/infra/http/controllers/UsersControllers';
@@ -11,7 +12,17 @@ const usersControllers = new UsersControllers();
 const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersControllers.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersControllers.create,
+);
 
 usersRouter.patch(
   '/avatar',
